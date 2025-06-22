29746fd6beb44464ea1861fe7902249239392aad
Hieu Luong <minhhieudn98@gmail.com>
2025-06-23 02:48:34 +0700

    Add nav layout

--- src/components/NavLayout.vue
+++ src/components/NavLayout.vue
@@ -2,7 +2,7 @@
   <div class="relative top-0">
     <div class="w-[100%] h[60px] fixed bg-black z-20 flex items-center justify-between" id="TopNav">
       <div class="flex items-center">
-        <button class="p-2 ml-3 rounded-full hover:bg-gray-500 cursor-pointer">
+        <button class="p-2 ml-3 rounded-full hover:bg-gray-500 cursor-pointer" @click="tonggleSideNav">
           <MenuIcon fillcolor="#ffffff" :size="26"/>
         </button>
         <div class="mx-2"></div>
@@ -25,12 +25,21 @@
         <img src="https://placehold.co/400" class="rounded-full mx-8" width="35" alt="Profile"/>
       </div>
     </div>
+    <div class="h-[100%] fixed bg-black z-0" id="SideNav"
+      :class="[!opensideNav ? 'w-[70px]' : 'w-[240px]'], 'hidden sm:block'">
+      <ul class="sm:mt-[60px] mt-[30px] w-full" :class="[!opensideNav? 'p-2': 'px-5 pb-2 pt-[7px]']"></ul>
+    </div>
   </div>
 </template>
 
 <script setup>
   import MenuIcon from 'vue-material-design-icons/Menu.vue';
   import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
+  import { ref } from 'vue';
+  const opensideNav = ref(false);
+  const tonggleSideNav = () => {
+    opensideNav.value = !opensideNav.value;
+  }
 </script>
 
 <style lang="scss" scoped>
