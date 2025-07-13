791f3530e4f6e525aa81d9bba7f6b212c2761497
Hieu Luong <minhhieudn98@gmail.com>
2025-07-14 02:18:44 +0700

    update recommend card

--- src/components/RecomendationCard.vue
+++ src/components/RecomendationCard.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col lg:flex-row md:gap-3 sm:items-start mt-[15px]">
-    <div class="relative lg:w-full">
+    <div class="relative lg:w-full flex md:flex-row flex-col gap-2 sm:gap-3">
       <img
         src="https://placehold.co/600x400"
-        class="aspect-video cursor-pointer rounded-lg sm:w-full"
+        class="aspect-video cursor-pointer rounded-lg w-full sm:w-[50%]"
         alt=""
       />
-      <div class="px-3 mt-2 sm:mt-3 ml-2 sm:w-1/2 text-white">
+      <div class="md:px-1 px-3 mt-2 sm:mt-3 ml-2 sm:w-1/2 text-white">
         <div class="text-[15px] pb-1.5 font-bold w-full cursor-pointer">
           Sample video Title
         </div>
